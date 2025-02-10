6fa352a443e4ac6a1d400ed42f1800214ca10841
密度跃层 <pycnocline@hotmail.com>
2025-02-11 02:01:38 +0800

    实现首页净化，只显示视频流
    去除了首页出视频流之外的所有内容，去除了视频流中的直播，广告，推送等

---
+++ scripts/homepage_filter.js
@@ -0,0 +1,33 @@
+// 获取首页信息流视频卡片
+const videoCards = document.querySelectorAll(".enable-no-interest");
+
+// 清空当前页面内容
+document.body.innerHTML = "";
+
+// 创建一个容器来装载这些元素
+const container = document.createElement("div");
+// 设置容器样式
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.gap = "20px";
+container.style.padding = "20px";
+container.style.justifyContent = "center";
+container.style.width = "100%";
+
+// 遍历视频卡片
+videoCards.forEach(card => {
+    // 克隆原始div标签
+    const cloneCard = card.cloneNode(true);
+
+    // 设置克隆卡片的样式
+    cloneCard.style.width = "600px";
+    cloneCard.style.height = "100%";
+    cloneCard.style.overflow = "hidden";
+    cloneCard.style.borderRadius = "5px";
+
+    // 将克隆的div标签添加到容器中
+    container.appendChild(cloneCard);
+});
+
+// 将容器添加到页面中
+document.body.appendChild(container);
