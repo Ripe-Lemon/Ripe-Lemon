5e7bbb13464e76795b93d8dcc763a41eec9a6b22
密度跃层 <pycnocline@hotmail.com>
2025-03-19 19:32:36 +0800

    添加了可过滤的广告种类

--- src/videopage/videopage.js
+++ src/videopage/videopage.js
@@ -8,7 +8,7 @@ function initializeVideopage() {
     removeSideAd();
 }
 
-// 去除视频右侧广告
+// 去广告
 function removeSideAd() {
     // 去除视频右侧小广告
     let adCard = document.querySelector('.video-card-ad-small');
@@ -16,5 +16,13 @@ function removeSideAd() {
 
     // 去除视频右侧大广告
     adCard = document.querySelector('.slide-ad-exp');
+    adCard.style.display = "none";
+
+    // 去除视频底部条形广告
+    adCard = document.querySelector('.ad-floor-exp');
+    adCard.style.display = "none";
+
+    // 去除视频底部条形活动广告
+    adCard = document.querySelector('.activity-m-v1');
     adCard.style.display = "none";
 }
